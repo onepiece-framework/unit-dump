9414122638aec6f0eaf8c73347c3764d597842f6
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-03-17 00:49:38 +0900

    Fix: index.php - 2019, Add register_shutdown_function(). Auto register to webpack.

--- index.php
+++ index.php
@@ -8,4 +8,39 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+
+/** namespace
+ *
+ * @creation  2019-02-22
+ */
+namespace OP;
+
+//	...
 include(__DIR__.'/Dump.class.php');
+
+//	...
+register_shutdown_function(function(){
+	//	...
+	try{
+		//	...
+		$webpack = Unit::Instantiate('Webpack');
+		$webpack->Js ([__DIR__.'/mark', __DIR__.'/dump']);
+		$webpack->Css([__DIR__.'/mark', __DIR__.'/dump']);
+
+	}catch( \Exception $e ){
+		//	...
+		echo $e->getMessage();
+
+		//	...
+		echo '<script>';
+		include(__DIR__.'/mark.js');
+		include(__DIR__.'/dump.js');
+		echo '</script>';
+
+		//	...
+		echo '<style>';
+		include(__DIR__.'/mark.css');
+		include(__DIR__.'/dump.css');
+		echo '</style>';
+	};
+});
