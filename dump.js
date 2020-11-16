78ebe84fca175b02edb6916f9964a180f2bbfcf5
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2020-11-16 21:02:09 +0900

    DEL: dump.js | Arg() --> $OP.Arg()

--- dump.js
+++ dump.js
@@ -29,52 +29,6 @@ if( $OP === undefined ){
 	}
 
 	//	...
-	function Arg(arg){
-		//	...
-		var type = (arg === null) ? 'null': typeof arg;
-		var span = document.createElement('span');
-
-		//	...
-		span.innerText = (arg === null) ? 'null':arg;
-
-		//	...
-		span.classList.add('arg');
-		span.classList.add(type);
-
-		//	...
-		switch( type ){
-			case 'boolean':
-				span.classList.add( arg ? 'true':'false' );
-			break;
-			case 'string':
-				//	Empty or Number
-				if( arg.length === 0 || arg.match(/^[0-9]+$/) ){
-					span.classList.add('quote');
-				}else
-
-				//	Has white space.
-				if( arg.match(/\s/) ){
-					arg = arg.replace(/\t/g, '\\t');
-					arg = arg.replace(/\n/g, '\\n');
-					arg = arg.replace(/\r/g, '\\r');
-					span.innerText = arg;
-
-					//	...
-					var html = span.innerHTML;
-					html = html.replace(/ /g  ,'<span class="meta space">&nbsp;</span>');
-					html = html.replace(/\\t/g,'<span class="meta">\\t</span>');
-					html = html.replace(/\\n/g,'<span class="meta">\\n</span>');
-					html = html.replace(/\\r/g,'<span class="meta">\\r</span>');
-					span.innerHTML = html;
-				};
-			break;
-		};
-
-		//	...
-		return span;
-	};
-
-	//	...
 	function table(json){
 		var dump = document.createElement('table');
 		for(var index in json){
@@ -112,7 +66,7 @@ if( $OP === undefined ){
 			//	...
 			var span = document.createElement('span');
 				span.classList.add('arg');
-				span.appendChild( Arg(value) );
+				span.appendChild( $OP.Arg(value) );
 
 			//	...
 			var args = document.createElement('span');
